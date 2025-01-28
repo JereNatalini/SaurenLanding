92b4f905bf3dd0ef102a706592e70c3514aca985
JereNatalini <jerenatalini36@gmail.com>
2025-01-28 11:00:46 -0300

    cambio de BrowseRoute a HashRouter

--- landing-sauren/src/main.tsx
+++ landing-sauren/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
-import { BrowserRouter as Router } from 'react-router-dom'; // Importa Router
+import { HashRouter as Router } from 'react-router-dom'; // Cambia BrowserRouter por HashRouter
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
